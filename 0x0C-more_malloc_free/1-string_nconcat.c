bc143a36dd57f34674d153a4b4e7f18ff423a5ce
poshleks <adetunjahad@gmail.com>
2022-11-10 11:41:24 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,68 +1,51 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * *string_nconcat - concatenates two strings.
- *
- * @s1: first string.
- * @s2: second string.
- * @n: string length of whole or part of s2
- * Return: pointer to new string.
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_str;
-	unsigned int i = 0, j = 0, m, p;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	m = string_length(s1);
-	p = string_length(s2);
-	new_str = malloc(sizeof(char) * (m * n) + 1);
-	if (new_str == NULL)
-		return (NULL);
-	/* insert s1 into new_str */
-	while (s1[i] != '\0')
-	{
-		new_str[j] = s1[i];
-		i++;
-		j++;
-	}
-	/* insert s2 into new_str */
-	if (n >= p)
-	{
-		i = 0;
-		while (s2[i] != '\0')
-		{
-			new_str[j] = s2[i];
-			i++;
-			j++;
-		}
-	}
-	i = 0;
-	while ((i + 1) <= n && n <= p)
-	{
-		new_str[j] = s2[i];
-		i++;
-		j++;
-	}
-	new_str[j] = '\0';
-	return (new_str);
-}
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
 
-	while (*(pointer + c) != '\0')
+	while (s1[size1] != '\0')
 	{
-		c++;
+		size1++;
 	}
-	return (c);
+
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
