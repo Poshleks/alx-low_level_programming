90ff56811b37504cbc07497083b3f8f66dfea29d
poshleks <adetunjahad@gmail.com>
2022-12-05 21:31:55 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,35 @@
 #include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stddef.h>
+
 /**
- * read_textfile - reads a text file and prints stdout
- * @filename: Filename to be read
- * @letters: Number of letters to read and print
- * Return: Number of letters it could read and print
+ * read_textfile - reads a text file ans prints it to the POSIX standard output
+ *
+ * @filename: the path of the file
+ * @letters: the number of letters it should read and print
+ *
+ * Return: number of letters it could read and print
+ * or 0 if the file can't be opened, filename is NULL, or write fails
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t	read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fdopen, fdread, fdwrite;
-	char *buf;
+	int fd = open(filename, O_RDONLY);
+	ssize_t bytes = 0, write_o = -1;
+	char *buf = NULL;
 
-	fdopen = open(filename, O_RDWR);
-	buf = malloc(sizeof(char) * letters);
-
-	if (fdopen == -1 || filename == NULL)
-		return (0);
-	if (buf == NULL)
-		return (0);
-
-	fdread = read(fdopen, buf, letters);
-	fdwrite = write(STDOUT_FILENO, buf, fdread);
-	close(fdopen);
-	free(buf);
-
-	return (fdwrite);
+	if (fd > -1 && filename)
+	{
+		buf = malloc(sizeof(char) * (letters + 1));
+		if (buf)
+		{
+			bytes = read(fd, buf, letters);
+			buf[bytes] = '\0';
+			if (bytes > -1)
+				write_o = write(STDOUT_FILENO, buf, bytes);
+			free(buf);
+			close(fd);
+			if (write_o > -1)
+				return (bytes);
+		}
+	}
+	return (0);
 }
