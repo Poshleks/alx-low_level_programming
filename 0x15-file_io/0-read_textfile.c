3fafacae8207934be24ee2b4b9e00a20267a7739
poshleks <adetunjahad@gmail.com>
2022-12-05 21:37:01 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,46 @@
-#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file ans prints it to the POSIX standard output
+ * read_textfile - prints text from a file
  *
- * @filename: the path of the file
- * @letters: the number of letters it should read and print
+ * @filename: name of the file
+ * @letters: number of characters to read
  *
- * Return: number of letters it could read and print
- * or 0 if the file can't be opened, filename is NULL, or write fails
+ * Return: actual number of letters read, 0 if end of file
  */
-
-ssize_t	read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = open(filename, O_RDONLY);
-	ssize_t bytes = 0, write_o = -1;
-	char *buf = NULL;
+	int file;
+	int length, wrotechars;
+	char *buf;
 
-	if (fd > -1 && filename)
+	if (filename == NULL || letters == 0)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 	{
-		buf = malloc(sizeof(char) * (letters + 1));
-		if (buf)
-		{
-			bytes = read(fd, buf, letters);
-			buf[bytes] = '\0';
-			if (bytes > -1)
-				write_o = write(STDOUT_FILENO, buf, bytes);
-			free(buf);
-			close(fd);
-			if (write_o > -1)
-				return (bytes);
-		}
+		free(buf);
+		return (0);
 	}
-	return (0);
+	length = read(file, buf, letters);
+	if (length == -1)
+	{
+		free(buf);
+		close(file);
+		return (0);
+	}
+
+	wrotechars = write(STDOUT_FILENO, buf, length);
+
+	free(buf);
+	close(file);
+	if (wrotechars != length)
+		return (0);
+	return (length);
 }
